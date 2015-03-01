5c3ce70865c4a46232d4f488419198a140b16787
Curtis McEnroe <programble@gmail.com>
2015-03-01 00:29:44 -0500

    Add tests for Files.listMigrations

--- test/files.js
+++ test/files.js
@@ -1,14 +1,17 @@
 'use strict';
 
 var FS = require('fs');
+var Path = require('path');
 var assert = require('assert');
 
+var R = require('ramda');
 var Promise = require('bluebird');
 Promise.promisifyAll(FS);
 var mockFS = require('mock-fs');
 
 var Files = require('../lib/files');
 
+var assertFalse = R.partial(assert, false);
 var hooks = {
   mockFS: function(config) {
     return function() {
@@ -125,5 +128,198 @@ describe('Files', function() {
     });
 
     after(hooks.restoreFS);
+  });
+
+  describe('listMigrations', function() {
+    describe('with empty directory', function() {
+      before(hooks.mockFS({migrations: {}}));
+
+      it('succeeds', function() {
+        return this.migrations = Files.listMigrations('migrations');
+      });
+      it('returns empty array', function() {
+        return this.migrations.tap(function(migrations) {
+          assert(Array.isArray(migrations));
+          assert.equal(migrations.length, 0);
+        });
+      });
+
+      after(hooks.restoreFS);
+    });
+
+    describe('with migration files', function() {
+      before(hooks.mockFS({
+        migrations: {
+          '1.first.sql': 'CREATE TABLE a (a INTEGER);\n---\nDROP TABLE a;\n',
+          '2.second.sql': 'CREATE TABLE b (b INTEGER);\n---\nDROP TABLE b;\n'
+        }
+      }));
+
+      it('succeeds', function() {
+        return this.migrations = Files.listMigrations('migrations');
+      });
+      it('returns array of migrations sorted by ID', function() {
+        return this.migrations.tap(function(migrations) {
+          assert(Array.isArray(migrations));
+          assert.equal(migrations.length, 2);
+          assert(migrations[0].id < migrations[1].id);
+        });
+      });
+      it('returns migration IDs', function() {
+        return this.migrations.tap(function(migrations) {
+          assert.equal(migrations[0].id, '1');
+          assert.equal(migrations[1].id, '2');
+        });
+      });
+      it('returns migration names', function() {
+        return this.migrations.tap(function(migrations) {
+          assert.equal(migrations[0].name, 'first');
+          assert.equal(migrations[1].name, 'second');
+        });
+      });
+      it('returns migration split false', function() {
+        return this.migrations.each(function(migration) {
+          assert.equal(migration.split, false);
+        });
+      });
+      it('returns migration paths', function() {
+        return this.migrations.tap(function(migrations) {
+          assert.equal(migrations[0].path, Path.join('migrations', '1.first.sql'));
+          assert.equal(migrations[1].path, Path.join('migrations', '2.second.sql'));
+        });
+      });
+
+      after(hooks.restoreFS);
+    });
+
+    describe('with non-migration files', function() {
+      before(hooks.mockFS({
+        migrations: {
+          '1.test.sql': 'CREATE TABLE a (a INTEGER);\n',
+          '1.test.left.sql': 'invalid name',
+          '1.sql': 'missing name',
+          'test.sql': 'missing id',
+          '1.test.txt': 'not sql'
+        }
+      }));
+
+      it('succeeds', function() {
+        return this.migrations = Files.listMigrations('migrations');
+      });
+      it('does not return non-migrations', function() {
+        return this.migrations.tap(function(migrations) {
+          assert.equal(migrations.length, 1);
+        });
+      });
+
+      after(hooks.restoreFS);
+    });
+
+    describe('with split migration files', function() {
+      before(hooks.mockFS({
+        migrations: {
+          '1.first.up.sql': 'CREATE TABLE a (a INTEGER);\n',
+          '1.first.down.sql': 'DROP TABLE a;\n',
+          '2.second.up.sql': 'CREATE TABLE b (b INTEGER);\n',
+          '2.second.down.sql': 'DROP TABLE b;\n'
+        }
+      }));
+
+      it('succeeds', function() {
+        return this.migrations = Files.listMigrations('migrations');
+      });
+      it('returns array of migrations sorted by ID', function() {
+        return this.migrations.tap(function(migrations) {
+          assert(Array.isArray(migrations));
+          assert.equal(migrations.length, 2);
+          assert(migrations[0].id < migrations[1].id);
+        });
+      });
+      it('returns migration IDs', function() {
+        return this.migrations.tap(function(migrations) {
+          assert.equal(migrations[0].id, '1');
+          assert.equal(migrations[1].id, '2');
+        });
+      });
+      it('returns migration names', function() {
+        return this.migrations.tap(function(migrations) {
+          assert.equal(migrations[0].name, 'first');
+          assert.equal(migrations[1].name, 'second');
+        });
+      });
+      it('returns migration split true', function() {
+        return this.migrations.each(function(migration) {
+          assert.equal(migration.split, true);
+        });
+      });
+      it('returns migration up paths', function() {
+        return this.migrations.tap(function(migrations) {
+          assert.equal(migrations[0].upPath, Path.join('migrations', '1.first.up.sql'));
+          assert.equal(migrations[1].upPath, Path.join('migrations', '2.second.up.sql'));
+        });
+      });
+      it('returns migration down paths', function() {
+        return this.migrations.tap(function(migrations) {
+          assert.equal(migrations[0].downPath, Path.join('migrations', '1.first.down.sql'));
+          assert.equal(migrations[1].downPath, Path.join('migrations', '2.second.down.sql'));
+        });
+      });
+
+      after(hooks.restoreFS);
+    });
+
+    describe('with missing split migration file', function() {
+      before(hooks.mockFS({
+        migrations: {
+          '1.test.up.sql': 'CREATE TABLE a (a INTEGER);\n'
+        }
+      }));
+
+      it('throws SplitFileMissingError', function() {
+        return Files.listMigrations('migrations')
+          .then(assertFalse, function(error) {
+            assert(error instanceof Files.SplitFileMissingError);
+          });
+      });
+
+      after(hooks.restoreFS);
+    });
+
+    describe('with two split migration files of same type', function() {
+      before(hooks.mockFS({
+        migrations: {
+          '1.test.up.sql': 'CREATE TABLE a (a INTEGER);\n',
+          '1.what.up.sql': 'CREATE TABLE a (b INTEGER);\n'
+        }
+      }));
+
+      it('throws SplitFileMissingError', function() {
+        return Files.listMigrations('migrations')
+          .then(assertFalse, function(error) {
+            assert(error instanceof Files.SplitFileMissingError);
+          });
+      });
+
+      after(hooks.restoreFS);
+    });
+
+    describe('with conflicting migration files', function() {
+      before(hooks.mockFS({
+        migrations: {
+          '1.test.sql': 'CREATE TABLE a (a INTEGER);\n---\nDROP TABLE a;\n',
+          '1.test.up.sql': 'CREATE TABLE a (a INTEGER);\n',
+          '1.test.down.sql': 'DROP TABLE a;\n'
+        }
+      }));
+
+      it('throws SplitFileConflictError', function() {
+        return Files.listMigrations('migrations')
+          .then(assertFalse, function(error) {
+            assert(error instanceof Files.SplitFileConflictError);
+          });
+      });
+
+      after(hooks.restoreFS);
+    });
   });
 });
