f5993e6cc9a94e750b5db44a293cc7a4d72d37e5
Curtis McEnroe <programble@gmail.com>
2015-05-06 13:08:11 -0400

    Re-align pg and sqlite3 declarations

--- declaration/sqlite3.d.ts
+++ declaration/sqlite3.d.ts
@@ -6,83 +6,82 @@
 /// <reference path="node.d.ts" />
 /// <reference path="bluebird.d.ts" />
 
-declare module "sqlite3" {
-    import events = require("events");
-    
-    export var OPEN_READONLY: number;
-    export var OPEN_READWRITE: number;
-    export var OPEN_CREATE: number;
-    
-    export var cached: {
-        Database(filename: string, callback?: (err: Error) => void): Database;
-        Database(filename: string, mode?: number, callback?: (err: Error) => void): Database;
-    };
-    
-    export interface RunResult {
-        lastID: number;
-        changes: number;
-    }
-    
-    export class Statement {
-        public bind(callback?: (err: Error) => void): Statement;
-        public bind(...params: any[]): Statement;
-        
-        public reset(callback?: (err: Error) => void): Statement;
-        
-        public finalize(callback?: (err: Error) => void): Statement;
-        
-        public run(callback?: (err: Error) => void): Statement;
-        public run(...params: any[]): Statement;
-        
-        public get(callback?: (err: Error, row: any) => void): Statement;
-        public get(...params: any[]): Statement;
-        
-        public all(callback?: (err: Error, rows: any[]) => void): Statement;
-        public all(...params: any[]): Statement;
-        
-        public each(callback?: (err: Error, row: any) => void, complete?: (err: Error, count: number) => void): Statement;
-        public each(...params: any[]): Statement;
-    }
-    
-    export class Database extends events.EventEmitter {
-        constructor(filename: string, callback?: (err: Error) => void);
-        constructor(filename: string, mode?: number, callback?: (err: Error) => void);
-        
-        public close(callback?: (err: Error) => void): void;
-        public closeAsync(): Promise<void>;
-        
-        public run(sql: string, callback?: (err: Error) => void): Database;
-        public run(sql: string, ...params: any[]): Database;
-        public runAsync(sql: string): Promise<void>;
-        public runAsync(sql: string, ...params: any[]): Promise<void>;
-        
-        public get(sql: string, callback?: (err: Error, row: any) => void): Database;
-        public get(sql: string, ...params: any[]): Database;
-        
-        public all(sql: string, callback?: (err: Error, rows: any[]) => void): Database;
-        public all(sql: string, ...params: any[]): Database;
-        public allAsync(sql: string): Promise<any[]>;
-        public allAsync(sql: string, ...params: any[]): Promise<any[]>;
-        
-        public each(sql: string, callback?: (err: Error, row: any) => void, complete?: (err: Error, count: number) => void): Database;
-        public each(sql: string, ...params: any[]): Database;
-        
-        public exec(sql: string, callback?: (err: Error) => void): Database;
-        public execAsync(sql: string): Promise<void>;
-        
-        public prepare(sql: string, callback?: (err: Error) => void): Statement;
-        public prepare(sql: string, ...params: any[]): Statement;
-        
-        public serialize(callback?: () => void): void;
-        public parallelize(callback?: () => void): void;
-        
-        public on(event: "trace", listener: (sql: string) => void): Database;
-        public on(event: "profile", listener: (sql: string, time: number) => void): Database;
-        public on(event: "error", listener: (err: Error) => void): Database;
-        public on(event: "open", listener: () => void): Database;
-        public on(event: "close", listener: () => void): Database;
-        public on(event: string, listener: Function): Database;
-    }
-    
-    function verbose(): void;
+declare module 'sqlite3' {
+  import events = require('events');
+
+  export var OPEN_READONLY: number;
+  export var OPEN_READWRITE: number;
+  export var OPEN_CREATE: number;
+
+  export var cached: {
+    Database(filename: string, callback?: (err: Error) => void): Database;
+    Database(filename: string, mode?: number, callback?: (err: Error) => void): Database;
+  };
+
+  export interface RunResult {
+    lastID: number;
+    changes: number;
+  }
+
+  export class Statement {
+    bind(callback?: (err: Error) => void): Statement;
+    bind(...params: any[]): Statement;
+
+    reset(callback?: (err: Error) => void): Statement;
+
+    finalize(callback?: (err: Error) => void): Statement;
+
+    run(callback?: (err: Error) => void): Statement;
+    run(...params: any[]): Statement;
+
+    get(callback?: (err: Error, row: any) => void): Statement;
+    get(...params: any[]): Statement;
+
+    all(callback?: (err: Error, rows: any[]) => void): Statement;
+    all(...params: any[]): Statement;
+
+    each(callback?: (err: Error, row: any) => void, complete?: (err: Error, count: number) => void): Statement;
+    each(...params: any[]): Statement;
+  }
+
+  export class Database extends events.EventEmitter {
+    constructor(filename: string, callback?: (err: Error) => void);
+    constructor(filename: string, mode?: number, callback?: (err: Error) => void);
+
+    close(callback?: (err: Error) => void): void;
+    closeAsync(): Promise<void>;
+
+    run(sql: string, callback?: (err: Error) => void): Database;
+    run(sql: string, ...params: any[]): Database;
+    runAsync(sql: string, ...params: any[]): Promise<void>;
+
+    get(sql: string, callback?: (err: Error, row: any) => void): Database;
+    get(sql: string, ...params: any[]): Database;
+    getAsync(sql: string, ...params: any[]): Promise<any>;
+
+    all(sql: string, callback?: (err: Error, rows: any[]) => void): Database;
+    all(sql: string, ...params: any[]): Database;
+    allAsync(sql: string, ...params: any[]): Promise<any[]>;
+
+    each(sql: string, callback?: (err: Error, row: any) => void, complete?: (err: Error, count: number) => void): Database;
+    each(sql: string, ...params: any[]): Database;
+
+    exec(sql: string, callback?: (err: Error) => void): Database;
+    execAsync(sql: string): Promise<void>;
+
+    prepare(sql: string, callback?: (err: Error) => void): Statement;
+    prepare(sql: string, ...params: any[]): Statement;
+
+    serialize(callback?: () => void): void;
+    parallelize(callback?: () => void): void;
+
+    on(event: 'trace', listener: (sql: string) => void): Database;
+    on(event: 'profile', listener: (sql: string, time: number) => void): Database;
+    on(event: 'error', listener: (err: Error) => void): Database;
+    on(event: 'open', listener: () => void): Database;
+    on(event: 'close', listener: () => void): Database;
+    on(event: string, listener: Function): Database;
+  }
+
+  function verbose(): void;
 }
