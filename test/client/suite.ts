e1152d93c03d43b4c875fc37dfd5ff0ddc7c0674
Curtis McEnroe <programble@gmail.com>
2015-05-08 19:07:39 -0400

    Increase hook timeouts on create/drop database

--- test/client/suite.ts
+++ test/client/suite.ts
@@ -20,12 +20,18 @@ function suite<T extends client.Client, U extends client.Config>(t: Suite<T, U>)
   var db: client.Connection;
 
   var hooks = {
-    createDatabase:   () => t.createDatabase().tap(c => config = c),
+    createDatabase: function() {
+      this.timeout(5000);
+      return t.createDatabase().tap(c => config = c);
+    },
+    dropDatabase: function() {
+      this.timeout(5000);
+      return t.dropDatabase(config);
+    },
     connect:          () => t.client.connect(config).tap(c => db = c),
     beginTransaction: () => t.client.beginTransaction(db),
     ensureJournal:    () => t.client.ensureJournal(db, 'journal'),
     disconnect:       () => t.client.disconnect(db),
-    dropDatabase:     () => t.dropDatabase(config)
   };
 
   (t.skip ? describe.skip : describe)(t.prettyName + ' client', function() {
