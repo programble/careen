b34e6670e97738762f6b0bc62f728f5cd1e9da65
Curtis McEnroe <programble@gmail.com>
2015-05-16 22:17:09 -0400

    Add declarations for Sinon Stub and Mock APIs

--- declaration/sinon.d.ts
+++ declaration/sinon.d.ts
@@ -77,4 +77,85 @@ declare module 'sinon' {
   function spy(): Spy;
   function spy(myFunc: Function): Spy;
   function spy(object: Object, method: string): Spy;
+
+  interface Stub extends Spy {
+    withArgs(...args: any[]): Stub;
+
+    onCall(n: number): Stub;
+    onFirstCall(): Stub;
+    onSecondCall(): Stub;
+    onThirdCall(): Stub;
+
+    returns(obj: any): Stub;
+    returnsArg(index: number): Stub;
+    returnsThis(): Stub;
+
+    throws(): Stub;
+    throws(name: string): Stub;
+    throws(obj: Object): Stub;
+
+    callsArg(index: number): Stub;
+    callsArgOn(index: number, context: any): Stub;
+    callsArgWith(index: number, ...args: any[]): Stub;
+    callsArgOnWith(index: number, context: any, ...args: any[]): Stub;
+
+    yields(...args: any[]): Stub;
+    yieldsOn(context: any, ...args: any[]): Stub;
+    yieldsTo(property: string, ...args: any[]): Stub;
+    yieldsToOn(property: string, context: any, ...args: any[]): Stub;
+
+    yield(...args: any[]): Stub;
+    yieldTo(callback: string, ...args: any[]): Stub;
+
+    callArg(argNum: number): Stub;
+    callArgWith(argNum: number, ...args: any[]): Stub;
+
+    callsArgAsync(index: number): Stub;
+    callsArgOnAsync(index: number, context: any): Stub;
+    callsArgWithAsync(index: number, ...args: any[]): Stub;
+    callsArgOnWithAsync(index: number, context: any, ...args: any[]): Stub;
+
+    yieldsAsync(...args: any[]): Stub;
+    yieldsOnAsync(context: any, ...args: any[]): Stub;
+    yieldsToAsync(property: string, ...args: any[]): Stub;
+    yieldsToOnAsync(property: string, context: any, ...args: any[]): Stub;
+  }
+
+  function stub(): Stub;
+  function stub(object: Object): Stub;
+  function stub(object: Object, method: string, func?: Function): Stub;
+
+  interface Mock {
+    expects(method: string): Expectation;
+
+    restore(): void;
+    verify(): void;
+  }
+
+  interface Expectation extends Stub {
+    atLeast(number: number): Expectation;
+    atMost(number: number): Expectation;
+
+    never(): Expectation;
+
+    once(): Expectation;
+    twice(): Expectation;
+    thrice(): Expectation;
+
+    exactly(number: number): Expectation;
+
+    withArgs(...args: any[]): Expectation;
+    withExactArgs(...args: any[]): Expectation;
+
+    on(obj: any): Expectation;
+
+    verify(): void;
+  }
+
+  function mock(obj: Object): Mock;
+  function mock(): Expectation;
+
+  module expectation {
+    function create(methodName: string): Expectation;
+  }
 }
